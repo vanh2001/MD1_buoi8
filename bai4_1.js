12e36ac290134bbe622d150a15e52a3d0f0f9075
vanh2001 <anhviet1709@gmail.com>
2022-01-05 22:46:02 +0700

    Add bai luyen tap switch case

---
+++ bai4_1.js
@@ -0,0 +1,14 @@
+let browser = prompt('Mời bạn nhập tên trình duyệt');
+switch (browser) {
+    case 'Edge':
+        alert("You've got the Edge!");
+        break;
+    case 'Chrome':
+    case 'Firebox':
+    case 'Safari':
+    case 'Opera':
+        alert("Okay we support these browsers too");
+        break;
+    default:
+        alert("We hope that this page looks ok!")
+}
---
+++ bai4_3.js
@@ -0,0 +1,22 @@
+let value = prompt('Type of number: ',0);
+//if-else
+// if(value > 0){
+//     alert(1);
+// }else if (value < 0){
+//     alert(-1);
+// }else {
+//     alert(0);
+// }
+
+//switch-case
+switch (value) {
+    case (value > 0):
+        alert(1);
+        break;
+    case (value < 0):
+        alert(-1);
+        break;
+    default:
+        alert(0);
+        break;
+}
