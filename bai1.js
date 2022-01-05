0ccc6671390f242624cf25cbc9e4e664b136d31c
vanh2001 <anhviet1709@gmail.com>
2022-01-05 21:42:08 +0700

    Add bai Nam Nhuan

---
+++ bai1.js
@@ -0,0 +1,15 @@
+let year;
+year = Number(prompt("Mời bạn nhập năm vào: "));
+if(year % 4 == 0){
+    if (year % 100 == 0){
+        if (year % 400 == 0){
+            alert(year + 'là năm nhuận');
+        }else {
+            alert(year + 'không là năm nhuận');
+        }
+    }else {
+        alert(year + 'không là năm nhuận');
+    }
+}else {
+    alert(year + 'Không là năm nhuận');
+}
