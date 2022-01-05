b34aebb8964494b86f2e03e385e4a9821d3395ad
vanh2001 <anhviet1709@gmail.com>
2022-01-05 22:06:18 +0700

    Add answer ECMAScript

---
+++ bai2.js
@@ -0,0 +1,6 @@
+let answer = prompt('What is the "offical" name of JavaScript?','');
+if(answer == "ECMAScript"){
+    alert('Right!You are done!');
+}else {
+    alert("Didn't know? ECMAScript");
+}
